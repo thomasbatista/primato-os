56e66fcabfba0e0de6eb7f708c17ec33e7e63859
Thomas Batista <139007458+thomasbatista@users.noreply.github.com>
2026-07-21 11:40:56 -0300

    chore(backend): scaffold spring boot project skeleton

---
+++ backend/src/main/java/com/primatoos/backend/PrimatoOsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.primatoos.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimatoOsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimatoOsBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/primatoos/backend/PrimatoOsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primatoos.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimatoOsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
